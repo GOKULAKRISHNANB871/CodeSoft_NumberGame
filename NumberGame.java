931269a184d0562746a282083b453c90dd8df9f7
GOKULAKRISHNAN B (B TECH IT) <gokulakrishnanb145@gmail.com>
2024-08-22 21:30:37 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Number{
+public class NumberGame{
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner input = new Scanner(System.in);
